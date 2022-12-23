caf26af8c957717c842f427cdbedc0bee9a5ed72
ushmz <44394399+ushmz@users.noreply.github.com>
2022-12-23 15:20:43 +0900

    feat: add button to copy URL

--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchResult.module.css";
+import { Clipboard } from "react-feather";
 
 type SearchResultProps = {
   title: string;
@@ -10,10 +11,30 @@ type SearchResultProps = {
 };
 
 export const SearchResult: React.FC<SearchResultProps> = (props) => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const showTipOnCopied = () => {
+    setVisible(true);
+    setTimeout(() => setVisible(false), 1000);
+  };
+
   return (
     <div className={styles.container} onMouseEnter={props.sendHoverLog}>
-      <a href={props.url} target="_blank" rel="noreferrer" onClick={props.sendClickLog}>
+      <div className={visible ? "visible" : "invisible"}>
+        <div className="tooltip tooltip-open tooltip-top" data-tip="URLをコピーしました" />
+      </div>
+      <span className="flex">
+        <button
+          className={styles.copybtn}
+          onClick={() => {
+            navigator.clipboard.writeText(props.url);
+            showTipOnCopied();
+          }}
+        >
+          <Clipboard className="h-5" />
+        </button>
         <div className={styles.url}>{props.url}</div>
+      </span>
+      <a href={props.url} target="_blank" rel="noreferrer" onClick={props.sendClickLog}>
         <h3 className={styles.title}>{props.title}</h3>
       </a>
       <div className={styles.snippet}>{props.snippet.slice(0, 80) + "..."}</div>
